467827af9e3a4e98f8c5d2f31549c312eaba2546
Alenorze <alenorze@gmail.com>
2017-11-15 17:35:44 +0200

    INITIAL

---
+++ restang/restang/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+admin.autodiscover()
+
+
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^admin/', include(admin.site.urls)),
+]
