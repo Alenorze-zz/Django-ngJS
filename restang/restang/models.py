82ca549bef34481731b8971c8a0301d7df34ddc9
Алексей Белов <Alenorze@gmail.com>
2017-12-15 03:04:00 +0200

    Full FIX

--- restang/restang/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class RestangElements(models.Model):
-    restang_text = models.CharField(max_length=200)
-    done = models.BooleanField()
-
-
--- restang/restang/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from restang.models import RestangElements
-
-class RestangSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RestangElements
-        fields = 'id', 'restang_text', 'done'
